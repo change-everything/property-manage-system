5a941a9515f5bb5dc2645b1e147b49e9082d99f1
裴永鹏 <1158336066@qq.com>
2022-06-07 18:45:46 +0800

    2022.6.7pyp第二次提交

---
+++ property-management-system/src/main/java/edu/f4/config/WebConfig.java
@@ -0,0 +1,29 @@
+package edu.f4.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author PeiYP
+ * @since 2022年06月07日 17:37
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+
+}
