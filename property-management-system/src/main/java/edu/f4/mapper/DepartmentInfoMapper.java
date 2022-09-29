6d7c8d7881fdc06b8cd4e80ce605a52914eb1ba8
裴永鹏 <1158336066@qq.com>
2022-09-29 19:23:02 +0800

    :fix: 修复了数据显示错误

--- property-management-system/src/main/java/edu/f4/mapper/DepartmentInfoMapper.java
+++
@@ -1,9 +0,0 @@
-package edu.f4.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import edu.f4.pojo.DepartmentInfo;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface DepartmentInfoMapper extends BaseMapper<DepartmentInfo> {
-}
--- property-management-system/src/main/java/edu/f4/service/IDepartmentInfoService.java
+++
@@ -1,8 +0,0 @@
-package edu.f4.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import edu.f4.pojo.DepartmentInfo;
-
-public interface IDepartmentInfoService extends IService<DepartmentInfo> {
-    boolean deleteDeptById(Integer deptNum);
-}
